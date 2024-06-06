9b6bb030a9b150a2d95591cc763385c3cc6a29de
Odukoya Abdullahi Ademola <odukoyaabdullahi01@gmail.com>
2024-06-06 16:46:01 +0100

    added godotenv fix connection with fx

--- main.go
+++ main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"log"
 
 	"github.com/ADEMOLA200/Admin-App.git/app"
+	"github.com/joho/godotenv"
 	"go.uber.org/fx"
 )
 
 func main() {
+	if err := godotenv.Load(); err != nil {
+        log.Fatalf("Error loading .env file: %v", err)
+    }
 	fx.New(app.Module).Run()
 }
  
