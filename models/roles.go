b84746c4a13faabbf8dde0c9fb61fe9751fac370
Odukoya Abdullahi Ademola <odukoyaabdullahi01@gmail.com>
2024-06-07 21:47:06 +0100

    added permission route (#27)

--- models/roles.go
+++ models/roles.go
@@ -1,6 +1,7 @@
 package models
 
 type Role struct {
-	Id 		uint	`json:"id"`	
-	Name	string	`json:"name"`
+	Id 			uint			`json:"id"`	
+	Name		string			`json:"name"`
+	Permissions	[]Permissions	`json:"permissions" gorm:"many2many:role_permissions"`
 }
